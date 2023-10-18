7c2d2be1c507525cf8a4dd40bf804c34fc2827c4
Steven Siciliano <ssicij@farmingdale.edu>
2023-10-18 11:46:26 -0400

    These are changes for the lab due. Lab week 7.

--- src/main/java/com/mycompany/javafx_db_example/App.java
+++ src/main/java/com/mycompany/javafx_db_example/App.java
@@ -48,56 +48,62 @@ public class App extends Application {
             System.out.println("| To display all users,   press 'a' |");
             System.out.println("| To insert to the DB,    press 'i' |");
             System.out.println("| To query by name,       press 'q' |");
+            System.out.println("| To Edit,                press 'w' |");
+            System.out.println("| To delete,              press 'd' |");
             System.out.println("| To exit,                press 'e' |");
             System.out.println("===================================");
             System.out.print("Enter your choice: ");
             input = scan.next().charAt(0);
+            scan.useDelimiter("\n"); //fix input spaces
 
             switch (input) {
-                case 'g':
-                     launch(args); //GUI
-                    break;
-
-                case 'c':
-                    cdbop.connectToDatabase(); //Your existing method
-                    break;
-                case 'a':
-                    cdbop.listAllUsers(); //all users in DB
-                    break;
-
-                case 'i':
+                case 'g' -> launch(args); //GUI
+                case 'c' -> cdbop.connectToDatabase(); //Your existing method
+                case 'a' -> cdbop.listAllUsers(); //all users in DB
+                case 'i' -> {
                     System.out.print("Enter Name: ");
-                    String name = scan.next();
+                    String name = scan.nextLine();
                     System.out.print("Enter Email: ");
-                    String email = scan.next();
+                    String email = scan.nextLine();
                     System.out.print("Enter Phone: ");
-                    String phone = scan.next();
+                    String phone = scan.nextLine();
                     System.out.print("Enter Address: ");
-                    String address = scan.next();
+                    String address = scan.nextLine();
                     System.out.print("Enter Password: ");
-                    String password = scan.next();
-                    cdbop.insertUser(name, email, phone, address, password); //Your insertUser method
-                    break;
-                case 'q':
+                    String password = scan.nextLine();
+                    System.out.print("Enter Salary: ");
+                    int salary = scan.nextInt();
+                    cdbop.insertUser(name, email, phone, address, password, salary); //user method
+                }
+                case 'q' -> {
                     System.out.print("Enter the name to query: ");
                     String queryName = scan.next();
                     cdbop.queryUserByName(queryName); //Your queryUserByName method
-                    break;
-                case 'e':
-                    System.out.println("Exiting...");
-                    break;
-                default:
-                    System.out.println("Invalid option. Please try again.");
+                }
+                case 'e' -> System.out.println("Exiting...");
+                case 'w' -> // edits
+                        editUserDetails();
+                default -> System.out.println("Invalid option. Please try again.");
             }
+
             System.out.println(" ");
         } while (input != 'e');
 
         scan.close();
 
-       
+
     }
 
+    private static void editUserDetails() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the name of the user to edit: ");
+        String username = scanner.nextLine();
 
+        if (username.isEmpty()) {
+            System.out.println("Invalid username. Please try again.");
+        } else {
 
-
+            System.out.println("User details edited successfully for " + username);
+        }
+    }
 }
--- src/main/java/com/mycompany/javafx_db_example/db/ConnDbOps.java
+++ src/main/java/com/mycompany/javafx_db_example/db/ConnDbOps.java
@@ -16,21 +16,27 @@ import java.sql.Statement;
  * @author MoaathAlrajab
  */
 public class ConnDbOps {
-    
-    
+
+    final String MYSQL_SERVER_URL = "jdbc:mysql://csc311courseserversteve.mariadb.database.azure.com/";
+    final String DB_URL = "jdbc:mysql://csc311courseserversteve.mariadb.database.azure.com/CSC311DB";
+    final String USERNAME = "csc311admin@csc311courseserversteve";
+    final String PASSWORD = "Pizzabut44!";
+    private Object person;
+
     public  boolean connectToDatabase() {
         boolean hasRegistredUsers = false;
 
-        final String MYSQL_SERVER_URL = "jdbc:mysql://localhost/";
-        final String DB_URL = "jdbc:mysql://localhost:3306/DBname";
-        final String USERNAME = "username";
-        final String PASSWORD = "password";
-        //Class.forName("com.mysql.jdbc.Driver");
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
         try {
             //First, connect to MYSQL server and create the database if not created
             Connection conn = DriverManager.getConnection(MYSQL_SERVER_URL, USERNAME, PASSWORD);
             Statement statement = conn.createStatement();
-            statement.executeUpdate("CREATE DATABASE IF NOT EXISTS DBname");
+            statement.executeUpdate("CREATE DATABASE IF NOT EXISTS CSC311DB");
             statement.close();
             conn.close();
 
@@ -44,6 +50,7 @@ public class ConnDbOps {
                     + "phone VARCHAR(200),"
                     + "address VARCHAR(200),"
                     + "password VARCHAR(200) NOT NULL"
+                    + "salary INT"
                     + ")";
             statement.executeUpdate(sql);
 
@@ -69,9 +76,7 @@ public class ConnDbOps {
     }
 
     public  void queryUserByName(String name) {
-        final String DB_URL = "jdbc:mysql://localhost:3306/DBname";
-        final String USERNAME = "username";
-        final String PASSWORD = "password";
+
 
         try {
             Connection conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
@@ -97,21 +102,23 @@ public class ConnDbOps {
     }
 
     public  void listAllUsers() {
-        final String DB_URL = "jdbc:mysql://localhost:3306/DBname";
-        final String USERNAME = "username";
-        final String PASSWORD = "password";
+
 
         try {
             Connection conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
-            String sql = "SELECT * FROM users ";
+            String sql = "UPDATE users SET name=? ";
             PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            String name = null;
+            preparedStatement.setString(1, name);
+            String email = null;
+            preparedStatement.setString(2, email);
 
             ResultSet resultSet = preparedStatement.executeQuery();
 
             while (resultSet.next()) {
                 int id = resultSet.getInt("id");
-                String name = resultSet.getString("name");
-                String email = resultSet.getString("email");
+                name = resultSet.getString("name");
+                email = resultSet.getString("email");
                 String phone = resultSet.getString("phone");
                 String address = resultSet.getString("address");
                 System.out.println("ID: " + id + ", Name: " + name + ", Email: " + email + ", Phone: " + phone + ", Address: " + address);
@@ -124,10 +131,8 @@ public class ConnDbOps {
         }
     }
 
-    public  void insertUser(String name, String email, String phone, String address, String password) {
-        final String DB_URL = "jdbc:mysql://localhost:3306/DBname";
-        final String USERNAME = "username";
-        final String PASSWORD = "password";
+    public  void insertUser(String name, String email, String phone, String address, String password, int salary) {
+    //public void
 
         try {
             Connection conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
@@ -143,6 +148,28 @@ public class ConnDbOps {
 
             if (row > 0) {
                 System.out.println("A new user was inserted successfully.");
+            }
+
+            preparedStatement.close();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    //make delete here
+    public void deleteUserByName(String name) {
+        try {
+            Connection conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+            String sql = "DELETE FROM users WHERE name = ?";
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            preparedStatement.setString(1, name);
+
+            int rowsDeleted = preparedStatement.executeUpdate();
+
+            if (rowsDeleted > 0) {
+                System.out.println("User " + name + " deleted successfully.");
+            } else {
+                System.out.println("User " + name + " not found.");
             }
 
             preparedStatement.close();
